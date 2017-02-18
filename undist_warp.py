486a96eec2f066a38cfb03fb1f6769533caa86e1
annatyurkina <atnoon6@gmail.com>
2017-02-18 17:12:24 +0000

    line detection pipeline

--- undist_warp.py
+++ undist_warp.py
@@ -8,20 +8,20 @@ import color_transform
 import glob
 import matplotlib.cm as cm
 
-def get_perspective_transform_matrix():
+def get_perspective_transform_matrices():
     src = np.float32([[595.0,450.0],[259.0,687.0],[1056.0,687.0],[687.0,450.0]])
     dst = np.float32([[219.0,0.0],[219.0,720.0],[1091.0,720.0],[1091.0,0.0]])
     M = cv2.getPerspectiveTransform(src, dst)
-    return M;
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    return M, Minv;
 
 def warp(img, mtx, dist):
     udst = cv2.undistort(img, mtx, dist, None, mtx)
     cvt = color_transform.combine(udst)
     #cv2.imshow('cvt',cvt)
-    M = get_perspective_transform_matrix()
-    print(cvt.shape)
+    M, Minv = get_perspective_transform_matrices()
     warped = cv2.warpPerspective(cvt, M, (cvt.shape[1], cvt.shape[0]), flags=cv2.INTER_LINEAR)
-    return warped
+    return warped, udst
 
 def write_warped_test_images():
 
@@ -34,11 +34,11 @@ def write_warped_test_images():
     for test_fname in test_images_sl + test_images:
         i+=1
         img = cv2.imread(test_fname)
-        wrp = warp(img, mtx, dist)
+        wrp, udst = warp(img, mtx, dist)
         plt.imsave('./output_images/warped' + str(i) + '.png', wrp, cmap=cm.gray)
         #cv2.imwrite('./output_images/warped' + str(i) + '.jpg', wrp)
 
 
-calibrate.write_undistorted_test_images()
-color_transform.write_color_transformed_test_images()
-write_warped_test_images()
+#calibrate.write_undistorted_test_images()
+#color_transform.write_color_transformed_test_images()
+#write_warped_test_images()
